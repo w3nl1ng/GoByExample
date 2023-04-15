64b5c33ee6e0e5156f4fe03fafc785c7b7144717
w3nl1ng <quanjunxiong@gmail.com>
2023-04-15 13:27:21 +0800

    finish values example

---
+++ app/values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	fmt.Println("go" + "lang")
+
+	// integer and float
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("7.0 / 3.0 =", 7.0/3.0)
+
+	// boolean
+	fmt.Println(true || false)
+	fmt.Println(true && false)
+	fmt.Println(!true)
+}
