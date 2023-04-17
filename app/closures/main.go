25c472b0d42f6598ec44d689f6a535b18e6d1ce2
w3nl1ng <quanjunxiong@gmail.com>
2023-04-17 14:25:43 +0800

    finish closures functions mutiple-return-values range recursion and varidic-functions example

---
+++ app/closures/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func intSep() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSep()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInt := intSep()
+	fmt.Println(newInt())
+}
