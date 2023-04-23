7bb5946ee1f4fa7392bd58567d6b18f552ed9cbb
w3nl1ng <w3nl1ng@qq.com>
2023-04-23 16:37:17 +0800

    finish xml example

--- app/xml/main.go
+++ app/xml/main.go
@@ -7,15 +7,14 @@ import (
 
 type Plant struct {
 	XMLName xml.Name `xml:"plant"`
-	Id int           `xml:"id,attr"`
-	Name string      `xml:"name"`
-	Origin []string    `xml:"origin"`
+	Id      int      `xml:"id,attr"`
+	Name    string   `xml:"name"`
+	Origin  []string `xml:"origin"`
 }
 
-
-func (p Plant)String() string {
+func (p Plant) String() string {
 	return fmt.Sprintf("id=%v, name=%v, origin=%v",
-						p.Id, p.Name, p.Origin)
+		p.Id, p.Name, p.Origin)
 }
 
 func main() {
@@ -24,4 +23,26 @@ func main() {
 	out, _ := xml.MarshalIndent(coffee, " ", "  ")
 	fmt.Println(string(out))
 	fmt.Println(xml.Header + string(out))
+
+	var p Plant
+	if err := xml.Unmarshal(out, &p); err != nil {
+		panic(err)
+	}
+	fmt.Println(p)
+
+	tomato := &Plant{Id: 70, Name: "tomato", Origin: []string{
+		"memme",
+		"cjall",
+	}}
+
+	type Nesting struct {
+		XMLName xml.Name `xml:"nesting"`
+		Plants  []*Plant `xml:"parent>child>plant"`
+	}
+
+	nesting := &Nesting{}
+	nesting.Plants = []*Plant{&coffee, tomato}
+
+	out, _ = xml.MarshalIndent(nesting, " ", "  ")
+	fmt.Println(string(out))
 }
