036afad47d3f9b33471e352fee8dd3d32452c4b7
w3nl1ng <quanjunxiong@gmail.com>
2023-04-23 10:34:34 +0800

    finish regexp example

--- app/regexp/main.go
+++ app/regexp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"bytes"
 )
 
 func main() {
@@ -12,4 +13,26 @@ func main() {
 	r, _ := regexp.Compile("p([a-z]+)ch")
 
 	fmt.Println(r.MatchString("peach"))
+	fmt.Println(r.FindString("peach punch"))
+
+	fmt.Println("idx:", r.FindStringIndex("peach punch"))
+	fmt.Println(r.FindStringSubmatch("peach punch"))
+
+	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
+
+	fmt.Println(r.FindAllString("peach punch", -1))
+	fmt.Println(r.FindAllStringSubmatchIndex("peach punch", -1))
+
+	fmt.Println(r.FindAllString("peach punch panch", 2))
+
+	fmt.Println(r.Match([]byte("peach")))
+
+	r = regexp.MustCompile("p([a-z]+)ch")
+	fmt.Println("r:", r)
+
+	r.ReplaceAllString("a peach", "<fruit>")
+
+	in := []byte("a peach")
+	out := r.ReplaceAllFunc(in, bytes.ToUpper)
+	fmt.Println(string(out))
 }
